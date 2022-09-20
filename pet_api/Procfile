cdb6c83e784b509eff5f53086c8dee26644e5acd
Mariam Eissa <58251849+mariamkhaled99@users.noreply.github.com>
2022-09-20 17:01:37 +0200

    Add files via upload

---
+++ pet_api/Procfile
@@ -0,0 +1,2 @@
+web: gunicorn pet_api.wsgi 
+
---
+++ pet_api/api/urls.py
@@ -0,0 +1,46 @@
+from django.urls import path,include
+from . import views
+from .views import Postviewset,Locationviewset,Breedviewset,Categoryviewset
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('api_post',Postviewset)
+router.register('api_breed',Breedviewset)
+router.register('api_category',Categoryviewset)
+router.register('api_location',Locationviewset)
+
+
+
+urlpatterns = [
+    # breed
+    path('', include(router.urls)),
+    # ------------------------------------------
+    path('breeds/',views.get_data_breeds ),
+    path('breeds/<str:pk>/',views.get_data_breed ),
+    path('breed/create/',views.create_breed ),
+    path('breed/<str:pk>/update/',views.update_breed ),
+    path('breed/<str:pk>/delete/',views.delete_breed ),
+    # post
+    # -------------------------------------------
+    path('posts/',views.get_data_posts ),
+    path('posts/<str:pk>/',views.get_data_post ),
+    path('post/create/',views.create_post ),
+    path('posts/<str:pk>/update/',views.update_post ),
+    path('posts/<str:pk>/delete/',views.delete_post ),
+    # category
+    # -----------------------------------------------
+    path('categories/',views.get_data_Categories ),
+    path('categories/<str:pk>/',views.get_data_Category ),
+    path('category/create/',views.create_category ),
+    path('categories/<str:pk>/update/',views.update_category ),
+    path('categories/<str:pk>/delete/',views.delete_Category ),
+    # location
+    # ---------------------------------------------------
+    path('locations/',views.get_data_locations ),
+    path('locations/<str:pk>/',views.get_data_location ),
+    path('location/create/',views.create_location ),
+    path('locations/<str:pk>/update/',views.update_location ),
+    path('locations/<str:pk>/delete/',views.delete_location ),
+
+     
+]
